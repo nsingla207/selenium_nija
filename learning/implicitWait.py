59927742518613a19f56b4799d8311895f64b86a
SangwanCode <parvesh.sangwan@gmail.com>
2024-09-07 18:03:00 +0530

    added implicit wait file

---
+++ learning/implicitWait.py
@@ -0,0 +1,39 @@
+
+from datetime import date, datetime
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class learnWait:
+
+    def learnImplicitWait(self):
+
+        dirpath= os.path.join(os.path.dirname(__file__),"..")
+        print("********* Dir Path********")
+        print(dirpath)
+
+        filepath=os.path.join(os.path.abspath(dirpath),"DelayedElement.html")
+        print("########## File Path ###########")
+
+        print(filepath)
+
+        driver=webdriver.Chrome()
+        driver.implicitly_wait(3)
+        driver.implicitly_wait(1)
+        driver.get(filepath)
+        print(datetime.now())
+        element=driver.find_element(By.XPATH,"//input[@type='text']")
+        element.send_keys("test")
+        print(datetime.now())               
+        print(" input value is : ", element.get_attribute('value'))   
+
+
+
+obj=learnWait()
+obj.learnImplicitWait()
+
+
+
+        
